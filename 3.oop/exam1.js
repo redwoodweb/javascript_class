0b15a7f9f66dcd634612ef540e3d142b6d720d19
redwoodweb <redwooweb@naver.com>
2025-04-12 16:22:50 +0900

    test

---
+++ 3.oop/exam1.js
@@ -0,0 +1,16 @@
+/**
+ * 자바스크립트 객체
+ * 
+객체
+1. 사용자 정의 객체
+리터럴
+생성자 + prototype
+
+2.코어객체 javascript 에서 제공하는 객체
+Array,Math,Number..
+
+3.browser object
+window, navigator, document...
+4.DOM
+
+ */
