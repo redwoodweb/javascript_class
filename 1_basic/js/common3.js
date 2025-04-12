8020bf89f291293331d16cf7884f89176f27ec2e
meekyoung <meeares17@gmail.com>
2025-04-12 13:39:33 +0900

    common3 updated

--- 1_basic/js/common3.js
+++ 1_basic/js/common3.js
@@ -1,2 +1,23 @@
 console.log( 5===5);
 console.log( 5==='5');
+
+/**
+ * for i, for of
+ */
+const arryMem = ['a','b','c'];
+
+// for(let key in arryMem){
+//   console.log(key);
+//   console.log(arryMem[key]);
+// }
+
+// for(let item of arryMem){
+//   console.log(item);
+// }
+
+// for(let index of arryMem){
+//   console.log(`${index}`);
+// }
+
+console.log('' || 'Dog');// 조건문 삼항 연산자와 같이 실행
+1 < 2 ? console.log('a'): console.log('2');
